8f5d4993d1f6d28f4e274ef3d0dfe50ce65d0cae
Levente Pato <plvnt2009@gmail.com>
2024-05-14 10:08:57 +0200

    Minor changes to the division-multiplication issue

--- Unittest/test_division.py
+++ Unittest/test_division.py
@@ -4,39 +4,42 @@ from helpers_division import factor_division_addition
 from helpers_division import factor_division_subtraction
 from helpers_division import factor_division_division
 from helpers_division import factor_division_multiplication
+from helpers_division import count_factors
 
-#class TestDivisionAddition(unittest.TestCase):
- #   def test_division_addition(self):
-  #      stunde = 5
-   #     while stunde < 24:
-    #        stunde = stunde + 1
-     #       x,y,z = factor_division_addition(stunde)
-      #      self.assertEqual(x/y+z, stunde)
+class TestDivisionAddition(unittest.TestCase):
+    def test_division_addition(self):
+        stunde = 4
+        while stunde < 24:
+            stunde = stunde + 1
+            x,y,z = factor_division_addition(stunde)
+            self.assertEqual(x/y+z, stunde)
 
-#class TestDivisionSubtraction(unittest.TestCase):
- #   def test_division_subtraktion(self):
-  #      stunde = 1
-   #     while stunde < 24:
-    #        stunde = stunde + 1
-     #       x,y,z = factor_division_subtraction(stunde)
-      #      self.assertEqual(x/y-z, stunde)
+class TestDivisionSubtraction(unittest.TestCase):
+    def test_division_subtraktion(self):
+        stunde = 1
+        while stunde < 24:
+            stunde = stunde + 1
+            x,y,z = factor_division_subtraction(stunde)
+            self.assertEqual(x/y-z, stunde)
 
-#class TestDivisionDivision(unittest.TestCase):
- #   def test_division_division(self):
-  #      stunde = 1        
-   #     while stunde < 24:
-    #        stunde = stunde + 1
-     #       x,y,z = factor_division_division(stunde)
-      #      self.assertEqual(x/y/z, stunde)
+class TestDivisionDivision(unittest.TestCase):
+    def test_division_division(self):
+        stunde = 1        
+        while stunde < 24:
+            stunde = stunde + 1
+            x,y,z = factor_division_division(stunde)
+            self.assertEqual(x/y/z, stunde)
 
 class TestDivisionMultiplication(unittest.TestCase):
     def test_division_multplication(self):
-        stunde = 5
+        stunde = 1
         while stunde < 24:
-            stunde = stunde + 1
+            while count_factors(stunde) < 3:    
+                stunde = stunde + 1
             x,y,z = factor_division_multiplication(stunde)
             self.assertEqual(x/y*z, stunde)
-            self.assertEqual()
+            stunde = stunde + 1
 
+        
 if __name__ == '__main__':
     unittest.main()
