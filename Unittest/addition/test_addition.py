b92aceb1bed4d32665730656d3dd90db52a08994
Levente Pato <plvnt2009@gmail.com>
2024-05-19 16:14:38 +0200

    [wip] Updated and adjusted the addition to the newer code versions. (no known bugs)

--- Unittest/addition/test_addition.py
+++ Unittest/addition/test_addition.py
@@ -3,40 +3,38 @@ from helpers_addition import factor_addition_addition
 from helpers_addition import factor_addition_subtraction
 from helpers_addition import factor_addition_division
 from helpers_addition import factor_addition_multiplication
-from helpers_addition import count_factors
 
 class TestAdditionAddition(unittest.TestCase):
     def test_addition_addition(self):
-        stunde = 6
+        stunde = 0
         while stunde < 24:
-            stunde = stunde + 1
             x,y,z = factor_addition_addition(stunde)
-            self.assertEqual(x+y+z, stunde)
-
+            if x*y*z > 1:
+                self.assertEqual(x+y+z, stunde)
+            stunde = stunde + 1
 class TestAdditionSubtraction(unittest.TestCase):
     def test_addition_subtraktion(self):
-        stunde = 2
+        stunde = 0
         while stunde < 24:
-            stunde = stunde + 1
             x,y,z = factor_addition_subtraction(stunde)
             self.assertEqual(x+y-z, stunde)
- 
+            stunde = stunde + 1
+
 class TestAdditionDivision(unittest.TestCase):
     def test_addition_division(self):
-        stunde = 3
+        stunde = 0
         while stunde < 24:
-            stunde = stunde + 1
             x,y,z = factor_addition_division(stunde)
-            self.assertEqual(x+y/z, stunde)
-
+            if x*y*z > 1:
+                self.assertEqual(y/z+x, stunde)
+            stunde = stunde + 1
 class TestAdditionMultiplication(unittest.TestCase):
     def test_addition_multplication(self):
-        stunde = 8
+        stunde = 0
         while stunde < 24:
-            while count_factors(stunde) < 3:    
-                stunde = stunde + 1
             x,y,z = factor_addition_multiplication(stunde)
-            self.assertEqual(x+y*z, stunde)
+            if x*y*z > 1:
+                self.assertEqual(x+y*z, stunde)
             stunde = stunde + 1
 
         
