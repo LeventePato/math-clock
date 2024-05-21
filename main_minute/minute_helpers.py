cffa3a0c66f17d5a0111c5f9b5d8266f7dae9714
Levente Pato <plvnt2009@gmail.com>
2024-05-21 22:35:30 +0200

    [wip] Reused stunde for minute in new file

--- main_minute/minute_helpers.py
+++ main_minute/minute_helpers.py
@@ -25,185 +25,185 @@ def teilbarkeit(n):                         #wird nur bei der doppelten Multipli
 
 # addition
 
-def factor_addition_addition(stunde):
-    if stunde < 6:
+def factor_addition_addition(minute):
+    if minute < 6:
         return 1, 1, 1
     else:
-        ergebnis_erste_rechnung = random.randrange(4, stunde - 1)
-        dritte_zahl = stunde - ergebnis_erste_rechnung
+        ergebnis_erste_rechnung = random.randrange(4, minute - 1)
+        dritte_zahl = minute - ergebnis_erste_rechnung
         erste_zahl = random.randrange(2, ergebnis_erste_rechnung - 1)
         zweite_zahl = ergebnis_erste_rechnung - erste_zahl
         return int(erste_zahl), int(zweite_zahl), int(dritte_zahl)
 
-def factor_addition_subtraction(stunde):
-    if stunde > 4:
-        ergebnis_erste_rechnung = random.randrange(stunde + 2, 100)
+def factor_addition_subtraction(minute):
+    if minute > 4:
+        ergebnis_erste_rechnung = random.randrange(minute + 2, 100)
     else:
         ergebnis_erste_rechnung = random.randrange(4, 100)
-    dritte_zahl = ergebnis_erste_rechnung - stunde
+    dritte_zahl = ergebnis_erste_rechnung - minute
     erste_zahl = random.randrange(2, ergebnis_erste_rechnung - 1)
     zweite_zahl = ergebnis_erste_rechnung - erste_zahl
     return int(erste_zahl), int(zweite_zahl), int(dritte_zahl)
 
-def factor_addition_division(stunde):
-    if stunde < 4:
+def factor_addition_division(minute):
+    if minute < 4:
         return 1, 1, 1
     else:
-        ergebnis_erste_rechnung = random.randrange(2, stunde - 1)
-        erste_zahl = stunde - ergebnis_erste_rechnung
+        ergebnis_erste_rechnung = random.randrange(2, minute - 1)
+        erste_zahl = minute - ergebnis_erste_rechnung
         zweite_zahl = random.randrange(ergebnis_erste_rechnung * 2, 100)
         while ergebnis_erste_rechnung not in faktoren[zweite_zahl]:
             zweite_zahl = random.randrange(ergebnis_erste_rechnung * 2, 100)
         dritte_zahl = zweite_zahl / ergebnis_erste_rechnung
         return int(erste_zahl), int(zweite_zahl), int(dritte_zahl)
 
-def factor_addition_multiplication(stunde):
-    if stunde < 6:
+def factor_addition_multiplication(minute):
+    if minute < 6:
         return 1, 1, 1
     else : 
-        ergebnis_erste_rechnung = random.randrange(4, stunde - 1)
+        ergebnis_erste_rechnung = random.randrange(4, minute - 1)
         while len(faktoren[ergebnis_erste_rechnung]) == 0:
-            ergebnis_erste_rechnung = random.randrange(4, stunde - 1)
-        erste_zahl = stunde - ergebnis_erste_rechnung 
+            ergebnis_erste_rechnung = random.randrange(4, minute - 1)
+        erste_zahl = minute - ergebnis_erste_rechnung 
         zweite_zahl = random.choice(faktoren[ergebnis_erste_rechnung])
         dritte_zahl = ergebnis_erste_rechnung / zweite_zahl
         return int(erste_zahl), int(zweite_zahl), int(dritte_zahl)
     
 # subtraktion
 
-def factor_subtraction_addition(stunde):
-    if stunde < 3:
+def factor_subtraction_addition(minute):
+    if minute < 3:
         return 1, 1, 1
     else: 
-        ergebnis_erste_rechnung = random.randrange(1, stunde - 1)
-        dritte_zahl = stunde - ergebnis_erste_rechnung
+        ergebnis_erste_rechnung = random.randrange(1, minute - 1)
+        dritte_zahl = minute - ergebnis_erste_rechnung
         erste_zahl = random.randrange(ergebnis_erste_rechnung + 2, 100)
         zweite_zahl = erste_zahl - ergebnis_erste_rechnung
         return int(erste_zahl), int(zweite_zahl), int(dritte_zahl)
 
-def factor_subtraction_subtraction(stunde):
-    ergebnis_erste_rechnung = random.randrange(stunde + 2, 97)
-    dritte_zahl = ergebnis_erste_rechnung - stunde
+def factor_subtraction_subtraction(minute):
+    ergebnis_erste_rechnung = random.randrange(minute + 2, 97)
+    dritte_zahl = ergebnis_erste_rechnung - minute
     erste_zahl = random.randrange(ergebnis_erste_rechnung + 2, 100)
     zweite_zahl = erste_zahl - ergebnis_erste_rechnung
     return int(erste_zahl), int(zweite_zahl), int(dritte_zahl)
 
-def factor_subtraction_division(stunde):
-    ergebnis_erste_rechnung = random.randrange(stunde + 1, 50)
-    erste_zahl = ergebnis_erste_rechnung + stunde
+def factor_subtraction_division(minute):
+    ergebnis_erste_rechnung = random.randrange(minute + 1, 50)
+    erste_zahl = ergebnis_erste_rechnung + minute
     zweite_zahl = random.randrange(ergebnis_erste_rechnung * 2, 100)
     while ergebnis_erste_rechnung not in faktoren[zweite_zahl]:
         zweite_zahl = random.randrange(ergebnis_erste_rechnung * 2, 100)
     dritte_zahl = zweite_zahl / ergebnis_erste_rechnung
     return int(erste_zahl), int(zweite_zahl), int(dritte_zahl)
 
-def factor_subtraction_multiplication(stunde):
-    ergebnis_erste_rechnung = random.randrange(stunde + 2, 100)
+def factor_subtraction_multiplication(minute):
+    ergebnis_erste_rechnung = random.randrange(minute + 2, 100)
     while ergebnis_erste_rechnung in prime_numbers:
-        ergebnis_erste_rechnung = random.randrange(stunde + 2, 100)
-    erste_zahl = ergebnis_erste_rechnung + stunde 
+        ergebnis_erste_rechnung = random.randrange(minute + 2, 100)
+    erste_zahl = ergebnis_erste_rechnung + minute 
     zweite_zahl = random.choice(faktoren[ergebnis_erste_rechnung])
     dritte_zahl = ergebnis_erste_rechnung / zweite_zahl
     return int(erste_zahl), int(zweite_zahl), int(dritte_zahl)
 
 # division
 
-def factor_division_addition(stunde):
-    if stunde < 4:
+def factor_division_addition(minute):
+    if minute < 4:
         return 1, 1, 1
     else:
-        ergebnis_erste_rechnung = random.randrange(2, stunde - 1)
-        erste_zahl = random.randrange(stunde * 2 + 2, 100)
+        ergebnis_erste_rechnung = random.randrange(2, minute - 1)
+        erste_zahl = random.randrange(minute * 2 + 2, 100)
         while ergebnis_erste_rechnung not in faktoren[erste_zahl]:
             erste_zahl = random.randrange(ergebnis_erste_rechnung * 2, 100)
         zweite_zahl = erste_zahl / ergebnis_erste_rechnung
-        dritte_zahl = stunde - ergebnis_erste_rechnung
+        dritte_zahl = minute - ergebnis_erste_rechnung
         return int(erste_zahl), int(zweite_zahl), int(dritte_zahl)
     
-def factor_division_subtraction(stunde):
-    ergebnis_erste_rechnung = random.randrange(int(stunde + 2), 49)
-    dritte_zahl = ergebnis_erste_rechnung - stunde
+def factor_division_subtraction(minute):
+    ergebnis_erste_rechnung = random.randrange(int(minute + 2), 49)
+    dritte_zahl = ergebnis_erste_rechnung - minute
     zweite_zahl = random.randrange(2, int(100 / ergebnis_erste_rechnung + 1))
     erste_zahl = ergebnis_erste_rechnung * zweite_zahl
     return int(erste_zahl), int(zweite_zahl), int(dritte_zahl)
 
-def factor_division_division(stunde):
-    if stunde < 1:
+def factor_division_division(minute):
+    if minute < 1:
         return 1, 1, 1
     else:
-        ergebnis_erste_rechnung = random.randrange(stunde * 2, 50)
-        while stunde not in faktoren[ergebnis_erste_rechnung] and stunde != 1:
-            ergebnis_erste_rechnung = random.randrange(stunde * 2, 50)
+        ergebnis_erste_rechnung = random.randrange(minute * 2, 50)
+        while minute not in faktoren[ergebnis_erste_rechnung] and minute != 1:
+            ergebnis_erste_rechnung = random.randrange(minute * 2, 50)
         erste_zahl = random.randrange(ergebnis_erste_rechnung * 2, 100)
         while ergebnis_erste_rechnung not in faktoren[erste_zahl]:
             erste_zahl = random.randrange(ergebnis_erste_rechnung * 2, 100)
         zweite_zahl = erste_zahl / ergebnis_erste_rechnung
-        dritte_zahl = ergebnis_erste_rechnung / stunde
+        dritte_zahl = ergebnis_erste_rechnung / minute
         return int(erste_zahl), int(zweite_zahl), int(dritte_zahl)
 
-def factor_division_multiplication(stunde):
-    if stunde < 4 or stunde in prime_numbers:
+def factor_division_multiplication(minute):
+    if minute < 4 or minute in prime_numbers:
         return 1, 1, 1
     else : 
-        ergebnis_erste_rechnung = random.choice(faktoren[stunde])
+        ergebnis_erste_rechnung = random.choice(faktoren[minute])
         erste_zahl = random.randrange(ergebnis_erste_rechnung * 2, 100)
         while ergebnis_erste_rechnung not in faktoren[erste_zahl]:
             erste_zahl = random.randrange(ergebnis_erste_rechnung * 2, 100)
         zweite_zahl = erste_zahl / ergebnis_erste_rechnung
-        dritte_zahl = stunde / ergebnis_erste_rechnung
+        dritte_zahl = minute / ergebnis_erste_rechnung
         return int(erste_zahl), int(zweite_zahl), int(dritte_zahl)
     
 # multiplikation
 
-def factor_multiplication_addition(stunde): 
-    if stunde < 6:
+def factor_multiplication_addition(minute): 
+    if minute < 6:
         return 1, 1, 1
     else: 
-        ergebnis_erste_rechnung = random.randrange(4, stunde - 1)
+        ergebnis_erste_rechnung = random.randrange(4, minute - 1)
         while ergebnis_erste_rechnung in prime_numbers:
-            ergebnis_erste_rechnung = random.randrange(4, stunde - 1)
+            ergebnis_erste_rechnung = random.randrange(4, minute - 1)
         erste_zahl = random.choice(faktoren[ergebnis_erste_rechnung])
         zweite_zahl = ergebnis_erste_rechnung / erste_zahl
-        dritte_zahl = stunde - ergebnis_erste_rechnung
+        dritte_zahl = minute - ergebnis_erste_rechnung
         return int(erste_zahl), int(zweite_zahl), int(dritte_zahl)
 
-def factor_multiplication_subtraction(stunde):
-    ergebnis_erste_rechnung = random.randrange(stunde + 2, 100)
+def factor_multiplication_subtraction(minute):
+    ergebnis_erste_rechnung = random.randrange(minute + 2, 100)
     while ergebnis_erste_rechnung in prime_numbers:
-        ergebnis_erste_rechnung = random.randrange(stunde + 2, 100)
+        ergebnis_erste_rechnung = random.randrange(minute + 2, 100)
     erste_zahl = random.choice(faktoren[ergebnis_erste_rechnung])
     zweite_zahl = ergebnis_erste_rechnung / erste_zahl
-    dritte_zahl = ergebnis_erste_rechnung - stunde
+    dritte_zahl = ergebnis_erste_rechnung - minute
     return int(erste_zahl), int(zweite_zahl), int(dritte_zahl)
 
 
-def factor_multiplication_division(stunde):
-    if stunde == 0:
+def factor_multiplication_division(minute):
+    if minute == 0:
         return 1, 1, 1
     else: 
-        ergebnis_erste_rechnung = random.randrange(stunde * 2, 100)
-        while ergebnis_erste_rechnung in prime_numbers or (stunde not in faktoren[ergebnis_erste_rechnung] and stunde != 1):
-            ergebnis_erste_rechnung = random.randrange(stunde * 2, 100)
+        ergebnis_erste_rechnung = random.randrange(minute * 2, 100)
+        while ergebnis_erste_rechnung in prime_numbers or (minute not in faktoren[ergebnis_erste_rechnung] and minute != 1):
+            ergebnis_erste_rechnung = random.randrange(minute * 2, 100)
         erste_zahl = random.choice(faktoren[ergebnis_erste_rechnung])
         zweite_zahl = ergebnis_erste_rechnung / erste_zahl
-        dritte_zahl = ergebnis_erste_rechnung / stunde 
+        dritte_zahl = ergebnis_erste_rechnung / minute 
         return int(erste_zahl), int(zweite_zahl), int(dritte_zahl)
 
-def factor_multiplication_multiplication(stunde):
-    if teilbarkeit(stunde) == False:
+def factor_multiplication_multiplication(minute):
+    if teilbarkeit(minute) == False:
         return 1, 1, 1
     else: 
-        ergebnis_erste_rechnung = random.randrange(4, int(stunde / 2) + 1)
-        while ergebnis_erste_rechnung in prime_numbers or ergebnis_erste_rechnung not in faktoren[stunde]:
-            ergebnis_erste_rechnung = random.randrange(4, int(stunde / 2) + 1)
+        ergebnis_erste_rechnung = random.randrange(4, int(minute / 2) + 1)
+        while ergebnis_erste_rechnung in prime_numbers or ergebnis_erste_rechnung not in faktoren[minute]:
+            ergebnis_erste_rechnung = random.randrange(4, int(minute / 2) + 1)
         erste_zahl = random.choice(faktoren[ergebnis_erste_rechnung])
         zweite_zahl = ergebnis_erste_rechnung / erste_zahl
-        dritte_zahl = stunde / ergebnis_erste_rechnung
+        dritte_zahl = minute / ergebnis_erste_rechnung
         return int(erste_zahl), int(zweite_zahl), int(dritte_zahl)
 
 
-def aufgabe_für_stunde(erster_operaror_für_stunde, zweiter_operator_für_stunde, erste_zahl, zweite_zahl, dritte_zahl):
-    erste_aufgabe = erste_zahl + erster_operaror_für_stunde + zweite_zahl
-    gesamte_aufgabe = erste_aufgabe + zweiter_operator_für_stunde + dritte_zahl
+def aufgabe_für_minute(erster_operaror_für_minute, zweiter_operator_für_minute, erste_zahl, zweite_zahl, dritte_zahl):
+    erste_aufgabe = erste_zahl + erster_operaror_für_minute + zweite_zahl
+    gesamte_aufgabe = erste_aufgabe + zweiter_operator_für_minute + dritte_zahl
     print(gesamte_aufgabe)
     return gesamte_aufgabe
