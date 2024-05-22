2d20e5e3a914f91a394c1afd84f129c5d877c236
Levente Pato <plvnt2009@gmail.com>
2024-05-22 22:49:20 +0200

    [wip] Finished merging minute with stunde in new file due to failure in the first one.

---
+++ Functions_main/main_test.py
@@ -0,0 +1,11 @@
+import helpers
+
+while True:
+    for stunde in range(0, 24):
+        for minute in range(0, 60):
+            hour_encoded = helpers.aufgabe_für_stunde(stunde)
+            minute_encoded = helpers.aufgabe_für_minute(minute)
+            print ("%d:%d" % (stunde, minute))
+            print ("%d%s%d%s%d h\n%d%s%d%s%d m" % (hour_encoded[0], hour_encoded[1], hour_encoded[2], hour_encoded[3], hour_encoded[4], minute_encoded[0], minute_encoded[1], minute_encoded[2], minute_encoded[3], minute_encoded[4]))
+            print("_______________")
+    
